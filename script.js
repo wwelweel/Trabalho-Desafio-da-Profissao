b1beeb8a359d925f9b82a981f2a8f1a73a8bb3c7
wwelweel <1139561@atitus.edu.br>
2026-06-10 10:48:04 -0300

    Site 1.1

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 document.querySelectorAll('a[href^="#"]').forEach(a=>a.onclick=e=>{e.preventDefault();document.querySelector(a.getAttribute('href')).scrollIntoView({behavior:'smooth'});});
+document.addEventListener('DOMContentLoaded',()=>{
+
+});
